cd59947f78c1f9479e6c9d1839c54e9d82ebf264
dynastygateway05 <157122576+dynastygateway05@users.noreply.github.com>
2025-07-11 23:48:35 +0800

    update links color

--- ftc-app/src/Sidebar.js
+++ ftc-app/src/Sidebar.js
@@ -1,56 +1,49 @@
-  import React, { useState } from "react";
-  import { Container, Navbar, Tabs, Tab } from 'react-bootstrap'
-  import Admin from "./Admin"
-  import ProductTabs from "./ProductTabs";
-  import Profile from './profile'
-  import QrCode from './QrCode'
-  import SignUpCashier from "./signupCashier";
-  import GenerateVouchers from "./Vouchers"
-  const Sidebar = ({ usertype }) => {
-    const [key, setKey] = useState(usertype == 'Manager' ? 'profile' : 'qrcode');
+import React, { useState } from "react";
+import { Tabs, Tab } from 'react-bootstrap';
+import Admin from "./Admin";
+import ProductTabs from "./ProductTabs";
+import Profile from './profile';
+import QrCode from './QrCode';
+import SignUpCashier from "./signupCashier";
+import GenerateVouchers from "./Vouchers";
+import './Sidebar.css';
 
-    return (
-      
-      usertype === 'Cashier' ?
-        <Tabs
-          id="controlled-tab-example"
-          activeKey={key}
-          onSelect={(k) => setKey(k)}
-          className="mb-3"
-        >
-        
-          <Tab eventKey="qrcode" title="Scan Order">
-            <QrCode />
-          </Tab>
-        </Tabs> : <Tabs
-          id="controlled-tab-example"
-          activeKey={key}
-          onSelect={(k) => setKey(k)}
-          className="mb-3"
-        >
-          <Tab eventKey="profile" title="Profile">
-            <Profile />
-          </Tab>
-          <Tab eventKey="admin" title="Add/Edit Profile">
-            <Admin />
-          </Tab>
-          <Tab eventKey="vouchers" title="Generate Vouchers">
-            <GenerateVouchers />
-          </Tab>
-          <Tab eventKey="cashier" title="Register New Cashier">
-            <SignUpCashier />
-          </Tab>
-          <Tab eventKey="product" title="Add Product">
-            <ProductTabs />
-          </Tab>
-          <Tab eventKey="qrcode" title="Scan Order">
-            <QrCode />
-          </Tab>
-        </Tabs>
-    )
+const Sidebar = ({ usertype }) => {
+  const [key, setKey] = useState(usertype === 'Manager' ? 'profile' : 'qrcode');
 
+  const renderTabs = () => {
+    const commonTabs = [
+      { eventKey: 'qrcode', title: 'Scan Order', component: <QrCode /> }
+    ];
 
+    if (usertype === 'Cashier') return commonTabs;
 
-  }
+    // Tabs specific for Manager
+    return [
+      { eventKey: 'profile', title: 'Profile', component: <Profile /> },
+      { eventKey: 'admin', title: 'Add/Edit Profile', component: <Admin /> },
+      { eventKey: 'vouchers', title: 'Generate Vouchers', component: <GenerateVouchers /> },
+      { eventKey: 'cashier', title: 'Register New Cashier', component: <SignUpCashier /> },
+      { eventKey: 'product', title: 'Add Product', component: <ProductTabs /> },
+      ...commonTabs
+    ];
+  };
 
-  export default Sidebar;
+  return (
+   <Tabs
+  id="sidebar-tabs"
+  activeKey={key}
+  onSelect={(k) => setKey(k)}
+  className="mb-3 custom-tabs"
+>
+
+      {renderTabs().map(({ eventKey, title, component }) => (
+        <Tab key={eventKey} eventKey={eventKey} title={title}>
+          {component}
+        </Tab>
+      ))}
+    </Tabs>
+  );
+};
+
+export default Sidebar;
